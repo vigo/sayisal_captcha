4a9489049b729abfdecd1a1a7e074fda5dfaa5ab
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2014-09-22 10:12:54 +0300

    docs added

--- lib/sayisal_captcha.rb
+++ lib/sayisal_captcha.rb
@@ -1,17 +1,22 @@
 require "sayisal_captcha/version"
 
+# Kullanılacak modül
 module SayisalCaptcha
   NUMBERS = ["Sıfır", "Bir", "İki", "Üç", "Dört", "Beş", "Altı", "Yedi", "Sekiz", "Dokuz"]
   OPERATORS = [:+, :*]
 
+  # Sayıyı Türkçe tekste çeviren method
   def self.number_to_text(number: 0)
     NUMBERS[number]
   end
+  
+  # Random numaraları üreten ve hem sayı hem tekst olarak döndüren method
   def self.generate_numbers
     numbers = [(0..9).to_a.sample, (0..9).to_a.sample]
     [numbers, numbers.map{ |n| number_to_text(number: n) }]
   end
   
+  # Esas kullanacağımız ana method
   def self.generate_question
     numbers = generate_numbers.first
     operation = OPERATORS.sample
