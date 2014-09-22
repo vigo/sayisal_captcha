dc03b59834315629fcfde0d22f1eef3794689e7a
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2014-09-22 10:04:02 +0300

    tests

--- lib/sayisal_captcha.rb
+++ lib/sayisal_captcha.rb
@@ -1,5 +1,29 @@
 require "sayisal_captcha/version"
 
 module SayisalCaptcha
-  # Your code goes here...
+  NUMBERS = ["Sıfır", "Bir", "İki", "Üç", "Dört", "Beş", "Altı", "Yedi", "Sekiz", "Dokuz"]
+  OPERATORS = [:+, :*]
+
+  def self.number_to_text(number: 0)
+    NUMBERS[number]
+  end
+  def self.generate_numbers
+    numbers = [(0..9).to_a.sample, (0..9).to_a.sample]
+    [numbers, numbers.map{ |n| number_to_text(number: n) }]
+  end
+  
+  def self.generate_question
+    numbers = generate_numbers.first
+    operation = OPERATORS.sample
+    first_number = numbers.first
+    last_number = numbers.last
+    result = first_number.method(operation).call(last_number)
+    {
+      first_number: first_number,
+      last_number: last_number,
+      operation: operation,
+      question: "#{first_number} #{operation.to_s} #{last_number}",
+      result: result
+    }
+  end
 end
---
+++ test/sayisal_captach_test.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+require 'sayisal_captcha'
+
+class SayisalCaptchaTest < Minitest::Test
+  def setup
+    @captcha = SayisalCaptcha
+  end
+  def test_number_to_text
+    assert_equal "Bir", @captcha.number_to_text(number: 1)
+    assert_equal "Sıfır", @captcha.number_to_text(number: 0)
+  end
+  
+  # [[7, 2], ["Yedi", "İki"]]
+  def test_generate_numbers
+    result = @captcha.generate_numbers
+    digits = result.first
+    texts = result.last
+    digits.each_with_index do |number, index|
+      assert_instance_of Fixnum, number
+      assert_equal texts[index], @captcha.number_to_text(number: number)
+    end
+  end
+  
+  def test_question
+    result = @captcha.generate_question
+    assert_match /\d (\*|\+) \d/, result[:question]
+  end
+end
